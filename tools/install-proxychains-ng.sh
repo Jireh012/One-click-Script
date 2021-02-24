4f7cb5f864701032ceed999f5fc269f8e6ee7aa3
Jireh <jeanxyz.012@gmail.com>
2021-02-24 17:31:25 +0800

    修改proxychains-ng一键安装脚本

--- tools/install-proxychains-ng.sh
+++ tools/install-proxychains-ng.sh
@@ -8,15 +8,9 @@
 #	Blog: https://www.lyile.cn/
 #=================================================
 
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[信息]${Font_color_suffix}"
-Error="${Red_font_prefix}[错误]${Font_color_suffix}"
-Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
-
 version=4.14
 
-install{
-echo -e "开始proxychains-ng,版本为："${version}
+echo -e "开始安装proxychains-ng,版本为："${version}
 wget https://github.com/rofl0r/proxychains-ng/archive/v$version.tar.gz
 tar xf v$version.tar.gz
 
@@ -27,4 +21,4 @@ tar xf v$version.tar.gz
  echo -e "proxychains-ng安装完成，配置文件为 /etc/proxychains.conf"
 )
 
-install
+rm -rf v$version.tar.gz proxychains-ng-$version
