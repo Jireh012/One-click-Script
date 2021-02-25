a0cf2a3662f3e7b087c949c936b2e0fa57233f8c
Jireh <jeanxyz.012@gmail.com>
2021-02-25 10:08:09 +0800

    add usage description

--- tools/install-proxychains-ng.sh
+++ tools/install-proxychains-ng.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Usage:
+#   wget --no-check-certificate https://raw.githubusercontent.com/Jireh012/One-click-Script/main/tools/install-proxychains-ng.sh && chmod +x install-proxychains-ng.sh && sh install-proxychains-ng.sh
 
 #=================================================
 #	System Required: CentOS/Debian/Ubuntu
@@ -15,50 +17,51 @@ Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
 Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 
-check_root(){
-	[[ $EUID != 0 ]] && echo -e "${Error} 当前非ROOT账号(或没有ROOT权限)，无法继续操作，请更换ROOT账号或使用 ${Green_background_prefix}sudo su${Font_color_suffix} 命令获取临时ROOT权限（执行后可能会提示输入当前账号的密码）。" && exit 1
+check_root() {
+  [[ $EUID != 0 ]] && echo -e "${Error} 当前非ROOT账号(或没有ROOT权限)，无法继续操作，请更换ROOT账号或使用 ${Green_background_prefix}sudo su${Font_color_suffix} 命令获取临时ROOT权限（执行后可能会提示输入当前账号的密码）。" && exit 1
 }
 #检查系统
-check_sys(){
-	if [[ -f /etc/redhat-release ]]; then
-		release="centos"
-	elif cat /etc/issue | grep -q -E -i "debian"; then
-		release="debian"
-	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-		release="ubuntu"
-	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-		release="centos"
-	elif cat /proc/version | grep -q -E -i "debian"; then
-		release="debian"
-	elif cat /proc/version | grep -q -E -i "ubuntu"; then
-		release="ubuntu"
-	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-		release="centos"
-    fi
-	#bit=`uname -m`
+check_sys() {
+  if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+  elif cat /etc/issue | grep -q -E -i "debian"; then
+    release="debian"
+  elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+    release="ubuntu"
+  elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+    release="centos"
+  elif cat /proc/version | grep -q -E -i "debian"; then
+    release="debian"
+  elif cat /proc/version | grep -q -E -i "ubuntu"; then
+    release="ubuntu"
+  elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+    release="centos"
+  fi
+  #bit=`uname -m`
 }
 
-install(){
+install() {
   echo -e "开始安装proxychains-ng,版本为："${version}
-wget https://github.com/rofl0r/proxychains-ng/archive/v$version.tar.gz
-tar xf v$version.tar.gz
+  wget https://github.com/rofl0r/proxychains-ng/archive/v$version.tar.gz
+  tar xf v$version.tar.gz
 
-if [ "${release}" == "centos" ]; then
+  if [ "${release}" == "centos" ]; then
     yum install -y gcc
-elif [ "${release}" == "ubuntu" ]; then
+  elif [ "${release}" == "ubuntu" ]; then
     apt install -y gcc
-elif [ "${release}" == "debian" ]; then
+  elif [ "${release}" == "debian" ]; then
     apt-get install -y gcc
-fi
+  fi
 
-(cd proxychains-ng-$version
- ./configure --prefix=/usr --sysconfdir=/etc
- make && make install
- make install-config
- echo -e "proxychains-ng安装完成，配置文件为 /etc/proxychains.conf"
-)
+  (
+    cd proxychains-ng-$version
+    ./configure --prefix=/usr --sysconfdir=/etc
+    make && make install
+    make install-config
+    echo -e "proxychains-ng安装完成，配置文件为 /etc/proxychains.conf"
+  )
 
-rm -rf v$version.tar.gz proxychains-ng-$version
+  rm -rf v$version.tar.gz proxychains-ng-$version
 }
 check_sys
 install
